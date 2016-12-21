cf3d6b2c6090f1e8d0a51d6a7381c3724d29adf7
shubh24 <shubhankar24@gmail.com>
2016-12-21 17:56:52 +0530

    motive over years done

--- motives.R
+++ motives.R
@@ -1,12 +1,27 @@
+library(ggplot2)
 df = read.csv("Serial Killers Data.csv", stringsAsFactors = TRUE)
 
 m_df = df[,c("Code", "Type", "MethodDescription", "DateDeath")]
-
 m_df$DateDeath = as.character(m_df$DateDeath)
 
 m_df$DateDeath[nchar(m_df$DateDeath) == 8] = substr(m_df$DateDeath[nchar(m_df$DateDeath) == 8], 5, 8)
 m_df$DateDeath[nchar(m_df$DateDeath) == 9] = substr(m_df$DateDeath[nchar(m_df$DateDeath) == 9],6,9)
 m_df$DateDeath[nchar(m_df$DateDeath) == 10] = substr(m_df$DateDeath[nchar(m_df$DateDeath) == 10], 7,10)
-
 m_df$DateDeath = as.numeric(m_df$DateDeath)
-m_df = subset(m_df, m_df$DateDeath <= 2016) #removing noise
+m_df = subset(m_df, m_df$DateDeath <= 2016 & m_df$DateDeath >= 1990) #removing noise
+
+motives = as.data.frame(cbind(as.integer(m_df$Code), m_df$DateDeath))
+colnames(motives) = c("Motive", "Year")
+motives$Year = as.factor(motives$Year)
+motives$Motive = as.factor(motives$Motive)
+
+motives$freq = 1
+motives_freq = aggregate(freq ~ Year+Motive, data = motives, FUN = sum)
+motives_list = c("Financial Gain","Attention","Enjoyment","Anger","Mental Illness","cult","Avoid arrest","Gang activity","Convenience","Wildwest Outlaw", "Multiple Motivations")
+
+ggplot(motives_freq, aes(Year, freq, group = Motive, col = Motive)) + 
+  geom_line()+
+  ggtitle("Motive Variance over the Years") +
+  labs(x = "Years", y = "Number of reported homicides")+
+  scale_color_hue(labels = motives_list)+
+  theme_bw()
