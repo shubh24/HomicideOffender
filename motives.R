151cd1710b462fd7013c6951b5721c29486d8bcd
shubh24 <shubhankar24@gmail.com>
2016-12-22 14:08:59 +0530

    one hot encoding of methods

--- motives.R
+++ motives.R
@@ -8,7 +8,27 @@ m_df$DateDeath[nchar(m_df$DateDeath) == 8] = substr(m_df$DateDeath[nchar(m_df$Da
 m_df$DateDeath[nchar(m_df$DateDeath) == 9] = substr(m_df$DateDeath[nchar(m_df$DateDeath) == 9],6,9)
 m_df$DateDeath[nchar(m_df$DateDeath) == 10] = substr(m_df$DateDeath[nchar(m_df$DateDeath) == 10], 7,10)
 m_df$DateDeath = as.numeric(m_df$DateDeath)
-m_df = subset(m_df, m_df$DateDeath <= 2016 & m_df$DateDeath >= 1990) #removing noise
+m_df = subset(m_df, m_df$DateDeath <= 2016 & m_df$DateDeath >= 1980) #removing noise
+
+#Separating methods
+m_df$MethodDescription = as.character(m_df$MethodDescription)
+m_df$methods = strsplit(m_df$MethodDescription, ",")
+
+for (i in 1:nrow(m_df)){
+  for (j in m_df[i, "methods"][[1]]){
+    
+    if (!(j == "")){
+      if (!(j %in% colnames(m_df))){
+        m_df[,j] = 0
+      }
+      m_df[i,j] = 1
+    }
+    
+  }
+}
+
+m_df$MethodDescription = NULL
+m_df$methods = NULL
 
 motives = as.data.frame(cbind(as.integer(m_df$Code), m_df$DateDeath))
 colnames(motives) = c("Motive", "Year")
@@ -25,3 +45,4 @@ ggplot(motives_freq, aes(Year, freq, group = Motive, col = Motive)) +
   labs(x = "Years", y = "Number of reported homicides")+
   scale_color_hue(labels = motives_list)+
   theme_bw()
+
